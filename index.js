eede8a4a7e0fc8d22adc0421ae96ca70fd93c53e
Peter deHaan <peter@deseloper.com>
2017-02-09 17:06:04 -0800

    Add 'plugin:security/recommended' config to plugin

--- index.js
+++ index.js
@@ -34,5 +34,27 @@ module.exports = {
     'detect-disable-mustache-escape': 0,
     'detect-object-injection': 0,
     'detect-new-buffer': 0
+  },
+  configs: {
+    recommended: {
+      plugins: [
+        'security'
+      ],
+      rules: {
+        'security/detect-buffer-noassert': 'warn',
+        'security/detect-child-process': 'warn',
+        'security/detect-disable-mustache-escape': 'warn',
+        'security/detect-eval-with-expression': 'warn',
+        'security/detect-new-buffer': 'warn',
+        'security/detect-no-csrf-before-method-override': 'warn',
+        'security/detect-non-literal-fs-filename': 'warn',
+        'security/detect-non-literal-regexp': 'warn',
+        'security/detect-non-literal-require': 'warn',
+        'security/detect-object-injection': 'warn',
+        'security/detect-possible-timing-attacks': 'warn',
+        'security/detect-pseudoRandomBytes': 'warn',
+        'security/detect-unsafe-regex': 'warn'
+      }
+    }
   }
 };
