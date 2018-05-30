9e9c6cae179f170d60dcaa95470b229a9bc1e006
Grant Murphy <grant.murphy@elastic.co>
2018-05-30 13:37:12 +1000

    Add babel-eslint and non-literal-require test

--- test/detect-non-literal-require.js
+++ test/detect-non-literal-require.js
@@ -1,17 +1,24 @@
 'use strict';
 
 const RuleTester = require('eslint').RuleTester;
+RuleTester.setDefaultConfig({
+    parser: 'babel-eslint'
+});
 const tester = new RuleTester();
-
 const ruleName = 'detect-non-literal-require';
-const invalid = 'var a = require(c)';
-
 
 tester.run(ruleName, require(`../rules/${ruleName}`), {
-  valid: [{ code: 'var a = require(\'b\')' }],
+  valid: [
+      { code: 'var a = require(\'b\')' },
+      { code: 'var a = require(`b`)' }
+  ],
   invalid: [
     {
-      code: invalid,
+      code: 'var a = require(c)',
+      errors: [{ message: 'Found non-literal argument in require' }]
+    },
+    {
+      code: 'var a = require(`${c}`)',
       errors: [{ message: 'Found non-literal argument in require' }]
     }
   ]
