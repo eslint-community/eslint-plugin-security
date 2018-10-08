b8c5542c867d56448781565d051687cf2bf9e9c3
Marconet <garrett.marconet@hbo.com>
2018-10-08 13:22:55 -0700

    add tests for new cases

--- test/detect-possible-timing-attacks.js
+++ test/detect-possible-timing-attacks.js
@@ -6,30 +6,71 @@ const tester = new RuleTester();
 const ruleName = 'detect-possible-timing-attacks';
 const Rule = require(`../rules/${ruleName}`);
 
-const valid = 'if (age === 5) {}';
-const invalidLeft = 'if (password === \'mypass\') {}';
-const invalidRigth = 'if (\'mypass\' === password) {}';
-
 
 // We only check with one string "password" and operator "==="
 // to KISS.
 
 tester.run(`${ruleName} (left side)`, Rule, {
-  valid: [{ code: valid }],
+  valid: [
+    { code: 'if (age === 5) {}' },
+    { code: 'if (password === 5) {}' },
+    { code: 'password.toString() === true' },
+    { code: 'if (user.password === true) {}' },
+    { code: 'if (user["password"] === true) {}' }
+  ],
   invalid: [
     {
-      code: invalidLeft,
+      code: 'if (password === \'mypass\') {}',
+      errors: [{ message: 'Potential timing attack, left side: true' }]
+    },
+    {
+      code: 'password.toString() === \'mypass\'',
+      errors: [{ message: 'Potential timing attack, left side: true' }]
+    },
+    {
+      code: 'if (user.password === \'mypass\') {}',
+      errors: [{ message: 'Potential timing attack, left side: true' }]
+    },
+    {
+      code: 'if (user["password"] === \'mypass\') {}',
+      errors: [{ message: 'Potential timing attack, left side: true' }]
+    },
+    {
+      code: 'if (password === getFromDatabase()) {}',
       errors: [{ message: 'Potential timing attack, left side: true' }]
     }
+
   ]
 });
 
 
 tester.run(`${ruleName} (right side)`, Rule, {
-  valid: [{ code: valid }],
+  valid: [
+    { code: 'if (5 === age) {}' },
+    { code: 'if (5 === password) {}' },
+    { code: 'true === password.toString()' },
+    { code: 'if (true === user.password) {}' },
+    { code: 'if (true === user["password"]) {}' }
+  ],
   invalid: [
     {
-      code: invalidRigth,
+      code: 'if (\'mypass\' === password) {}',
+      errors: [{ message: 'Potential timing attack, right side: true' }]
+    },
+    {
+      code: '\'mypass\' === password.toString()',
+      errors: [{ message: 'Potential timing attack, right side: true' }]
+    },
+    {
+      code: 'if (\'mypass\' === user.password) {}',
+      errors: [{ message: 'Potential timing attack, right side: true' }]
+    },
+    {
+      code: 'if (\'mypass\' === user["password"]) {}',
+      errors: [{ message: 'Potential timing attack, right side: true' }]
+    },
+    {
+      code: 'if (getFromDatabase() === password) {}',
       errors: [{ message: 'Potential timing attack, right side: true' }]
     }
   ]
