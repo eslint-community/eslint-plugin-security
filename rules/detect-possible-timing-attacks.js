51bbf248b9f7aa7abe8babe6be60450c77e11e59
Marconet <garrett.marconet@hbo.com>
2018-10-08 12:51:42 -0700

    add handling for literals, call expressions, and member expressions; made contains actually check if contains rather than if equal

--- rules/detect-possible-timing-attacks.js
+++ rules/detect-possible-timing-attacks.js
@@ -18,7 +18,19 @@ var keywords = '((' + [
     'hash'
 ].join(')|(') + '))';
 
-var re = new RegExp('^' + keywords + '$', 'im');
+var re = new RegExp('^.*' + keywords + '.*$', 'im');
+
+/**
+ * Maps type to what to handler function
+ * Returns true if it can have a timing attack, false otherwise
+ */
+const isVulnHandlers = {
+    'CallExpression': isVulnCallExpression,
+    'Identifier': isVulnIdentifier,
+    'Literal': isVulnLiteral,
+    'MemberExpression': isVulnMemberExpression,
+    // Not exhaustive, but covers most cases. Does not cover ConditionalExpressions, for example
+}
 
 function containsKeyword (node) {
     if (node.type === 'Identifier') {
@@ -29,28 +41,39 @@ function containsKeyword (node) {
     return
 }
 
+function isVulnerable(node) {
+    return node && node.type && isVulnHandlers.hasOwnProperty(node.type) && isVulnHandlers[node.type](node);
+}
+
+function isVulnCallExpression(node) {
+    return isVulnerable(node.callee);
+}
+
+function isVulnIdentifier (node) {
+    return containsKeyword(node);
+}
+
+function isVulnLiteral (node) {
+    // String is the only Literal that can have a secret in it
+    return typeof node.value === 'string';
+}
+
+function isVulnMemberExpression (node) {
+    // If anything along the path is prohibited, it could be problematic
+    // ex: password.toString() could be one, so could user.password
+    return isVulnerable(node.object) || isVulnerable(node.property);
+}
+
 module.exports = function(context) {
 
     "use strict";
 
     return {
-        "IfStatement": function(node) {
-            if (node.test && node.test.type === 'BinaryExpression') {
-                if (node.test.operator === '==' || node.test.operator === '===' || node.test.operator === '!=' || node.test.operator === '!==') {
+        "BinaryExpression": function(node) {
+            if (node.operator === '==' || node.operator === '===' || node.operator === '!=' || node.operator === '!==') {
 
-                    if (node.test.left) {
-                        var left = containsKeyword(node.test.left);
-                        if (left) {
-                            return context.report(node, "Potential timing attack, left side: " + left);
-                        }
-                    }
-
-                    if (node.test.right) {
-                        var right = containsKeyword(node.test.right);
-                        if (right) {
-                            return context.report(node, "Potential timing attack, right side: " + right);
-                        }
-                    }
+                if (isVulnerable(node.left) && isVulnerable(node.right)) {
+                    return context.report(node, "Potential timing attack");
                 }
             }
         }
