45247027396a99052c44294aa7186c9dab89a9cc
Marconet <garrett.marconet@hbo.com>
2018-10-08 13:22:44 -0700

    added back in left/right logic and allow either side to have a keyword

--- rules/detect-possible-timing-attacks.js
+++ rules/detect-possible-timing-attacks.js
@@ -20,10 +20,36 @@ var keywords = '((' + [
 
 var re = new RegExp('^.*' + keywords + '.*$', 'im');
 
-/**
- * Maps type to what to handler function
- * Returns true if it can have a timing attack, false otherwise
- */
+const containsKeywordHandlers = {
+    'CallExpression': containsKeywordCallExpression,
+    'Identifier': containsKeywordIdentifier,
+    'MemberExpression': containsKeywordMemberExpression,
+    // Not exhaustive, but covers most cases. Does not cover ConditionalExpressions, for example
+}
+
+function containsKeyword (node) {
+    if (node && node.type && containsKeywordHandlers.hasOwnProperty(node.type)) {
+        return containsKeywordHandlers[node.type](node);
+    }
+}
+
+function containsKeywordCallExpression (node) {
+    return containsKeyword(node.callee);
+}
+
+function containsKeywordIdentifier (node) {
+    return re.test(node.name);
+}
+
+function containsKeywordMemberExpression (node) {
+    if (node.property.type === 'Literal') {
+        // Only care about literals in member expressions
+        return containsKeyword(node.object) || re.test(node.property.value);
+    } else {
+        return containsKeyword(node.object) || containsKeyword(node.property);
+    }
+}
+
 const isVulnHandlers = {
     'CallExpression': isVulnCallExpression,
     'Identifier': isVulnIdentifier,
@@ -32,25 +58,16 @@ const isVulnHandlers = {
     // Not exhaustive, but covers most cases. Does not cover ConditionalExpressions, for example
 }
 
-function containsKeyword (node) {
-    if (node.type === 'Identifier') {
-        if (re.test(node.name)) {
-            return true;
-        }
-    }
-    return
-}
-
-function isVulnerable(node) {
+function isVulnerableType (node) {
     return node && node.type && isVulnHandlers.hasOwnProperty(node.type) && isVulnHandlers[node.type](node);
 }
 
-function isVulnCallExpression(node) {
-    return isVulnerable(node.callee);
+function isVulnCallExpression (node) {
+    return isVulnerableType(node.callee);
 }
 
 function isVulnIdentifier (node) {
-    return containsKeyword(node);
+    return true;
 }
 
 function isVulnLiteral (node) {
@@ -61,7 +78,7 @@ function isVulnLiteral (node) {
 function isVulnMemberExpression (node) {
     // If anything along the path is prohibited, it could be problematic
     // ex: password.toString() could be one, so could user.password
-    return isVulnerable(node.object) || isVulnerable(node.property);
+    return isVulnerableType(node.object) || isVulnerableType(node.property);
 }
 
 module.exports = function(context) {
@@ -72,8 +89,12 @@ module.exports = function(context) {
         "BinaryExpression": function(node) {
             if (node.operator === '==' || node.operator === '===' || node.operator === '!=' || node.operator === '!==') {
 
-                if (isVulnerable(node.left) && isVulnerable(node.right)) {
-                    return context.report(node, "Potential timing attack");
+                if (isVulnerableType(node.left) && isVulnerableType(node.right)) {
+                    if (containsKeyword(node.left)) {
+                        return context.report(node, "Potential timing attack, left side: true");
+                    } else if (containsKeyword(node.right)) {
+                        return context.report(node, "Potential timing attack, right side: true");
+                    }
                 }
             }
         }
