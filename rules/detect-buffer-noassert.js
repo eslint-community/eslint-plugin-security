67d1f989863f3d29576f84e2ff61b6aaadd290cb
Sabrina Brogren <sabrina@returntocorp.com>
2020-07-10 13:59:53 -0400

    Update detect-buffer-noassert.js
    Typo fix

--- rules/detect-buffer-noassert.js
+++ rules/detect-buffer-noassert.js
@@ -26,7 +26,7 @@ module.exports = function(context) {
         "readInt32BE",
         "readFloatLE",
         "readFloatBE",
-        "readDoubleL",
+        "readDoubleLE",
         "readDoubleBE"
     ];
 
