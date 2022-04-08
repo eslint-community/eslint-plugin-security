208019bad4f70a142ab1f0ea7238c37cb70d1a5a
Mark K <33289461+MarkKragerup@users.noreply.github.com>
2022-04-08 02:11:44 +0200

    feat(non-literal-require): support template literals (#81)
    * Add non-literal require TemplateLiteral support
    
    Currently following code triggers the detect-non-literal-require rule:
    
    ```js
    const foo = require(`bar`)
    ```
    
    However it would be more accurate (and less noisey) if it triggered on
    cases such as:
    
    ```js
    const foo = require(`${bar}`)
    ```
    
    * Add babel-eslint and non-literal-require test
    
    * ESLint >6 requires absolute path for ruletester parsers.
    
    https://github.com/eslint/eslint/blob/main/docs/user-guide/migrating-to-6.0.0.md#rule-tester-parser
    
    * chore: removed dependency on babel-eslint
    
    * chore: ran linting fix
    
    Co-authored-by: Grant Murphy <grant.murphy@elastic.co>

--- rules/detect-non-literal-require.js
+++ rules/detect-non-literal-require.js
@@ -21,10 +21,12 @@ module.exports = {
   },
   create: function(context) {
     return {
-      'CallExpression': function(node) {
+      'CallExpression': function (node) {
         if (node.callee.name === 'require') {
           const args = node.arguments;
-          if (args && args.length > 0 && args[0].type !== 'Literal') {
+          if (args && args.length > 0 &&
+          (args[0].type === 'TemplateLiteral' && args[0].expressions.length > 0) ||
+          (args[0].type !== 'TemplateLiteral' && args[0].type !== 'Literal')) {
             const token = context.getTokens(node)[0];
             return context.report(node, 'Found non-literal argument in require');
           }
--- test/detect-non-literal-require.js
+++ test/detect-non-literal-require.js
@@ -1,16 +1,23 @@
 'use strict';
 
 const RuleTester = require('eslint').RuleTester;
-const tester = new RuleTester();
+
+const tester = new RuleTester({ parserOptions: { ecmaVersion: 6 } });
 
 const ruleName = 'detect-non-literal-require';
-const invalid = 'var a = require(c)';
 
 tester.run(ruleName, require(`../rules/${ruleName}`), {
-  valid: [{ code: 'var a = require(\'b\')' }],
+  valid: [
+    { code: 'var a = require(\'b\')' },
+    { code: 'var a = require(`b`)' }
+  ],
   invalid: [
     {
-      code: invalid,
+      code: 'var a = require(c)',
+      errors: [{ message: 'Found non-literal argument in require' }]
+    },
+    {
+      code: 'var a = require(`${c}`)',
       errors: [{ message: 'Found non-literal argument in require' }]
     }
   ]
