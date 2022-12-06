08ba4764a83761f6f44cb28940923f1d25f88581
Bastian Gebhardt <github@buzz-t.eu>
2022-12-06 21:55:26 +0100

    feat: extend detect non literal fs filename (#92)
    * feat: rewritten detect-non-literal-fs-filename rule
    
    * detects multiple cases of possible imports of fs methods
    * many tests added to cover that cases
    
    * refactor: extract searching for ImportDeclaration and VariableDeclaration in utils
    
    * chore: added JSdoc, inlined fsPackagesNames, renamed sink terminology
    
    * chore: changed report params to object
    
    * chore: rename 'sink', remove '?.' for node 12
    
    Co-authored-by: Bastian Gebhardt <Bastian Gebhardtgithub@buzz-t.eu>

--- rules/detect-non-literal-fs-filename.js
+++ rules/detect-non-literal-fs-filename.js
@@ -5,12 +5,210 @@
 
 'use strict';
 
+const fsMetaData = require('./data/fsFunctionData.json');
+const funcNames = Object.keys(fsMetaData);
+const fsPackageNames = ['fs', 'node:fs', 'fs/promises', 'node:fs/promises', 'fs-extra'];
+
+const { getImportDeclaration, getVariableDeclaration } = require('./utils/import-utils');
+
+//------------------------------------------------------------------------------
+// Utils
+//------------------------------------------------------------------------------
+
+function getIndices(node, argMeta) {
+  return (argMeta || []).filter((argIndex) => node.arguments[argIndex].type !== 'Literal');
+}
+
+function generateReport({ context, node, packageName, methodName, indices }) {
+  if (!indices || indices.length === 0) {
+    return null;
+  }
+  return context.report({ node, message: `Found ${methodName} from package "${packageName}" with non literal argument at index ${indices.join(',')}` });
+}
+
+/**
+ * Detects:
+ * | var something = require('fs').readFile;
+ * | something(a);
+ */
+function detectOnRequireWithProperty({ context, methodName, node, program }) {
+  const declaration = getVariableDeclaration({
+    condition: (declaration) => declaration.init.parent.id.name === methodName,
+    hasObject: true,
+    methodName,
+    packageNames: fsPackageNames,
+    program,
+  });
+
+  if (!declaration) {
+    return null;
+  }
+
+  // we found the require for our method!
+  const fsFunction = declaration.init.property.name;
+  const packageName = declaration.init.object.arguments[0].value;
+  const fnName = declaration.init.property.name;
+
+  const indices = getIndices(node, fsMetaData[fsFunction]);
+
+  return generateReport({
+    context,
+    node,
+    packageName,
+    methodName: fnName,
+    indices,
+  });
+}
+
+/**
+ * Detects:
+ * | var something = require('fs');
+ * | something.readFile(c);
+ */
+function detectOnMethodCall({ context, node, program, methodName }) {
+  const declaration = getVariableDeclaration({
+    packageNames: fsPackageNames,
+    hasObject: false,
+    program,
+  });
+
+  if (!declaration) {
+    return null;
+  }
+
+  const indices = getIndices(node.parent, fsMetaData[methodName]);
+
+  return generateReport({
+    context,
+    node,
+    packageName: declaration.init.arguments[0].value,
+    methodName,
+    indices,
+  });
+}
+
+/**
+ * Detects:
+ * | var { readFile: something } = require('fs')
+ * | readFile(filename)
+ */
+function detectOnDestructuredRequire({ context, methodName, node, program }) {
+  const declaration = getVariableDeclaration({
+    condition: (declaration) => declaration && declaration.id && declaration.id.properties && declaration.id.properties.some((p) => p.value.name === methodName),
+    hasObject: false,
+    methodName,
+    packageNames: fsPackageNames,
+    program,
+  });
+
+  if (!declaration) {
+    return null;
+  }
+
+  const realMethodName = declaration.id.properties.find((p) => p.value.name === methodName).key.name;
+
+  const meta = fsMetaData[realMethodName];
+  const indices = getIndices(node, meta);
+
+  return generateReport({
+    context,
+    node,
+    packageName: declaration.init.arguments[0].value,
+    methodName: realMethodName,
+    indices,
+  });
+}
+
+/**
+ * Detects:
+ * | import { readFile as something } from 'fs';
+ * | something(filename);
+ */
+function detectOnDestructuredImport({ context, methodName, node, program }) {
+  const importDeclaration = getImportDeclaration({ methodName, packageNames: fsPackageNames, program });
+
+  const specifier = importDeclaration && importDeclaration.specifiers && importDeclaration.specifiers.find((s) => !!funcNames.includes(s.imported.name));
+
+  if (!specifier) {
+    return null;
+  }
+
+  const fnName = specifier.imported.name;
+  const meta = fsMetaData[fnName];
+  const indices = getIndices(node, meta);
+
+  return generateReport({
+    context,
+    node,
+    packageName: specifier.parent.source.value,
+    methodName: fnName,
+    indices,
+  });
+}
+
+/**
+ * Detects:
+ * | import * as something from 'fs';
+ * | something.readFile(c);
+ */
+function detectOnDefaultImport({ context, methodName, node, objectName, program }) {
+  if (!funcNames.includes(methodName)) {
+    return null;
+  }
+
+  const importDeclaration = getImportDeclaration({ methodName: objectName, packageNames: fsPackageNames, program });
+
+  if (!importDeclaration) {
+    return null;
+  }
+
+  const meta = fsMetaData[methodName];
+  const indices = getIndices(node.parent, meta);
+
+  return generateReport({
+    context,
+    node,
+    packageName: importDeclaration.source.value,
+    methodName,
+    indices,
+  });
+}
+
+/**
+ * Detects:
+ * | var something = require('fs').promises;
+ * | something.readFile(filename)
+ */
+function detectOnPromiseProperty({ context, methodName, node, objectName, program }) {
+  const declaration = program.body
+    .filter((entry) => entry.type === 'VariableDeclaration')
+    .flatMap((entry) => entry.declarations)
+    .find(
+      (declaration) =>
+        declaration.id.name === objectName &&
+        declaration.init.type === 'MemberExpression' &&
+        // package name is fs / fs-extra
+        fsPackageNames.includes(declaration.init.object.arguments[0].value)
+    );
+
+  if (!declaration) {
+    return null;
+  }
+  const meta = fsMetaData[methodName];
+  const indices = getIndices(node.parent, meta);
+
+  return generateReport({
+    context,
+    node,
+    packageName: declaration.init.object.arguments[0].value,
+    methodName,
+    indices,
+  });
+}
+
 //------------------------------------------------------------------------------
 // Rule Definition
 //------------------------------------------------------------------------------
-
-const fsMetaData = require('./data/fsFunctionData.json');
-const funcNames = Object.keys(fsMetaData);
 
 module.exports = {
   meta: {
@@ -24,30 +222,88 @@ module.exports = {
   },
   create: function (context) {
     return {
+      CallExpression: function (node) {
+        // readFile/open/... (but might be renamed)
+        const localMethodName = node.callee.name;
+
+        // don't check require. If all arguments are Literals, it's surely safe!
+        if (!localMethodName || localMethodName === 'require' || node.arguments.every((argument) => argument.type === 'Literal')) {
+          return;
+        }
+
+        // this only works, when imports are on top level!
+        const program = context.getAncestors()[0];
+
+        const requireReport = detectOnRequireWithProperty({
+          context,
+          methodName: localMethodName,
+          node,
+          program,
+        });
+        if (requireReport) {
+          return requireReport;
+        }
+
+        const destructuredRequireReport = detectOnDestructuredRequire({
+          context,
+          methodName: localMethodName,
+          node,
+          program,
+        });
+        if (destructuredRequireReport) {
+          return destructuredRequireReport;
+        }
+
+        const importReport = detectOnDestructuredImport({
+          context,
+          methodName: localMethodName,
+          node,
+          program,
+        });
+        if (importReport) {
+          return importReport;
+        }
+      },
       MemberExpression: function (node) {
-        const result = [];
-        if (funcNames.indexOf(node.property.name) !== -1) {
-          const meta = fsMetaData[node.property.name];
-          const args = node.parent.arguments;
-          meta.forEach((i) => {
-            if (args && args.length > i) {
-              if (args[i].type !== 'Literal') {
-                result.push(i);
-              }
-            }
-          });
+        const realMethodName = node.property.name; // readFile/open/... (not renamed)
+        const localObjectName = node.object.name; // fs/node:fs/... (but might be renamed)
+
+        // this only works, when imports are on top level!
+        const program = context.getAncestors()[0];
+
+        const methodCallReport = detectOnMethodCall({
+          context,
+          methodName: realMethodName,
+          node,
+          program,
+        });
+        if (methodCallReport) {
+          return methodCallReport;
         }
 
-        if (result.length > 0) {
-          return context.report({ node: node, message: `Found fs.${node.property.name} with non literal argument at index ${result.join(',')}` });
+        const defaultImportReport = detectOnDefaultImport({
+          program,
+          objectName: localObjectName,
+          methodName: realMethodName,
+          context,
+          node,
+        });
+
+        if (defaultImportReport) {
+          return defaultImportReport;
         }
 
-        /*
-              if (node.parent && node.parent.arguments && node.parent.arguments[index].value) {
-                  return context.report(node, 'found Buffer.' + node.property.name + ' with noAssert flag set true');
+        const promisePropertyReport = detectOnPromiseProperty({
+          context,
+          methodName: realMethodName,
+          node,
+          objectName: localObjectName,
+          program,
+        });
 
-              }
-              */
+        if (promisePropertyReport) {
+          return promisePropertyReport;
+        }
       },
     };
   },
--- test/detect-non-literal-fs-filename.js
+++ test/detect-non-literal-fs-filename.js
@@ -1,18 +1,132 @@
 'use strict';
 
 const RuleTester = require('eslint').RuleTester;
-const tester = new RuleTester();
-
-const invalid = 'var a = fs.open(c)';
+const tester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+});
 
 const ruleName = 'detect-non-literal-fs-filename';
 
 tester.run(ruleName, require(`../rules/${ruleName}`), {
-  valid: [{ code: 'var a = fs.open(\'test\')' }],
-  invalid: [
+  valid: [
     {
-      code: invalid,
-      errors: [{ message: 'Found fs.open with non literal argument at index 0' }]
-    }
-  ]
+      code: `var fs = require('fs');
+             var a = fs.open('test')`,
+    },
+    {
+      code: `var something = require('some');
+             var a = something.readFile(c);`,
+    },
+    {
+      code: `var something = require('fs').readFile, readFile = require('foo').readFile;
+             readFile(c);`,
+    },
+  ],
+  invalid: [
+    /// requires
+    {
+      code: `var something = require('fs');
+             var a = something.open(c);`,
+      errors: [{ message: 'Found open from package "fs" with non literal argument at index 0' }],
+    },
+    {
+      code: `var one = require('fs').readFile;
+             one(filename);`,
+      errors: [{ message: 'Found readFile from package "fs" with non literal argument at index 0' }],
+    },
+    {
+      code: `var one = require('node:fs').readFile;
+             one(filename);`,
+      errors: [{ message: 'Found readFile from package "node:fs" with non literal argument at index 0' }],
+    },
+    {
+      code: `var one = require('fs/promises').readFile;
+             one(filename);`,
+      errors: [{ message: 'Found readFile from package "fs/promises" with non literal argument at index 0' }],
+    },
+    {
+      code: `var something = require('fs/promises');
+             something.readFile(filename);`,
+      errors: [{ message: 'Found readFile from package "fs/promises" with non literal argument at index 0' }],
+    },
+    {
+      code: `var something = require('node:fs/promises');
+             something.readFile(filename);`,
+      errors: [{ message: 'Found readFile from package "node:fs/promises" with non literal argument at index 0' }],
+    },
+    {
+      code: `var something = require('fs-extra');
+             something.readFile(filename);`,
+      errors: [{ message: 'Found readFile from package "fs-extra" with non literal argument at index 0' }],
+    },
+    {
+      code: `var { readFile: something } = require('fs');
+             something(filename)`,
+      errors: [{ message: 'Found readFile from package "fs" with non literal argument at index 0' }],
+    },
+    //// imports
+    {
+      code: `import { readFile as something } from 'fs';
+             something(filename);`,
+      errors: [{ message: 'Found readFile from package "fs" with non literal argument at index 0' }],
+    },
+    {
+      code: `import { readFile as something } from 'node:fs';
+             something(filename);`,
+      errors: [{ message: 'Found readFile from package "node:fs" with non literal argument at index 0' }],
+    },
+    {
+      code: `import { readFile as something } from 'fs-extra';
+             something(filename);`,
+      errors: [{ message: 'Found readFile from package "fs-extra" with non literal argument at index 0' }],
+    },
+    {
+      code: `import { readFile as something } from 'fs/promises'
+             something(filename)`,
+      errors: [{ message: 'Found readFile from package "fs/promises" with non literal argument at index 0' }],
+    },
+    {
+      code: `import { readFile as something } from 'node:fs/promises'
+             something(filename)`,
+      errors: [{ message: 'Found readFile from package "node:fs/promises" with non literal argument at index 0' }],
+    },
+    {
+      code: `import * as something from 'fs';
+             something.readFile(filename);`,
+      errors: [{ message: 'Found readFile from package "fs" with non literal argument at index 0' }],
+    },
+    {
+      code: `import * as something from 'node:fs';
+             something.readFile(filename);`,
+      errors: [{ message: 'Found readFile from package "node:fs" with non literal argument at index 0' }],
+    },
+    /// promises
+    {
+      code: `var something = require('fs').promises;
+             something.readFile(filename)`,
+      errors: [{ message: 'Found readFile from package "fs" with non literal argument at index 0' }],
+    },
+    {
+      code: `var something = require('node:fs').promises;
+             something.readFile(filename)`,
+      errors: [{ message: 'Found readFile from package "node:fs" with non literal argument at index 0' }],
+    },
+    {
+      code: `var something = require('fs');
+             something.promises.readFile(filename)`,
+      errors: [{ message: 'Found readFile from package "fs" with non literal argument at index 0' }],
+    },
+    {
+      code: `var something = require('node:fs');
+             something.promises.readFile(filename)`,
+      errors: [{ message: 'Found readFile from package "node:fs" with non literal argument at index 0' }],
+    },
+    {
+      code: "var fs = require('fs');\nfs.readFile(`template with ${filename}`);",
+      errors: [{ message: 'Found readFile from package "fs" with non literal argument at index 0' }],
+    },
+  ],
 });
