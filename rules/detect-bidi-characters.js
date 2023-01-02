4294d29cca8af5c627de759919add6dd698644ba
Simone Sanfratello <simone.sanfra@gmail.com>
2023-01-02 21:41:42 +0100

    feat: detect-bidi-characters rule (#95)
    * Resolved conflicts in README
    
    * Embedded anti-trojan-source and removed from dependencies
    
    * Expanded README with an example
    
    * Changed onCodePath with Program
    
    * Improved code style as suggested in review
    
    * Added rough location estimation in the error report
    
    * feat: implement exact location for each bidi char
    
    * Renamed to detect-bidi-characters and fixed first line offset in comments
    
    * Added JSDoc in detectBidiCharacters
    
    * Fixed MD034 - Bare URL used
    
    * Apply suggestions from code review
    
    Co-authored-by: Liran Tal <liran.tal@gmail.com>
    
    Co-authored-by: Luciano Mammino <lucianomammino@gmail.com>
    Co-authored-by: Liran Tal <liran.tal@gmail.com>

---
+++ rules/detect-bidi-characters.js
@@ -0,0 +1,101 @@
+/**
+ * Detect trojan source attacks that employ unicode bidi attacks to inject malicious code
+ * @author Luciamo Mammino
+ * @author Simone Sanfratello
+ * @author Liran Tal
+ */
+
+'use strict';
+
+const dangerousBidiCharsRegexp = /[\u061C\u200E\u200F\u202A\u202B\u202C\u202D\u202E\u2066\u2067\u2068\u2069]/gu;
+
+/**
+ * Detects all the dangerous bidi characters in a given source text
+ *
+ * @param {object} options - Options
+ * @param {string} options.sourceText - The source text to search for dangerous bidi characters
+ * @param {number} options.firstLineOffset - The offset of the first line in the source text
+ * @returns {Array<{line: number, column: number}>} - An array of reports, each report is an
+ *    object with the line and column of the dangerous character
+ */
+function detectBidiCharacters({ sourceText, firstLineOffset }) {
+  const sourceTextToSearch = sourceText.toString();
+
+  const lines = sourceTextToSearch.split(/\r?\n/);
+
+  return lines.reduce((reports, line, lineIndex) => {
+    let match;
+    let offset = lineIndex == 0 ? firstLineOffset : 0;
+
+    while ((match = dangerousBidiCharsRegexp.exec(line)) !== null) {
+      reports.push({ line: lineIndex, column: offset + match.index });
+    }
+
+    return reports;
+  }, []);
+}
+
+function report({ context, node, tokens, message, firstLineOffset }) {
+  if (!tokens || !Array.isArray(tokens)) {
+    return;
+  }
+  tokens.forEach((token) => {
+    const reports = detectBidiCharacters({ sourceText: token.value, firstLineOffset: token.loc.start.column + firstLineOffset });
+
+    reports.forEach((report) => {
+      context.report({
+        node: node,
+        data: {
+          text: token.value,
+        },
+        loc: {
+          start: {
+            line: token.loc.start.line + report.line,
+            column: report.column,
+          },
+          end: {
+            line: token.loc.start.line + report.line,
+            column: report.column + 1,
+          },
+        },
+        message,
+      });
+    });
+  });
+}
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+module.exports = {
+  meta: {
+    type: 'error',
+    docs: {
+      description: 'Detects trojan source attacks that employ unicode bidi attacks to inject malicious code.',
+      category: 'Possible Security Vulnerability',
+      recommended: true,
+      url: 'https://github.com/eslint-community/eslint-plugin-security/blob/main/docs/rules/detect-bidi-characters.md',
+    },
+  },
+  create: function (context) {
+    return {
+      Program: function (node) {
+        report({
+          context,
+          node,
+          tokens: node.tokens,
+          firstLineOffset: 0,
+          message: "Detected potential trojan source attack with unicode bidi introduced in this code: '{{text}}'.",
+        });
+        report({
+          context,
+          node,
+          tokens: node.comments,
+          firstLineOffset: 2,
+          message: "Detected potential trojan source attack with unicode bidi introduced in this comment: '{{text}}'.",
+        });
+      },
+    };
+  },
+};
