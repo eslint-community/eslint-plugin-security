fc128018a0e607eb671a9798023c19877dc40b73
Eljees <3.14hell@gmail.com>
2026-07-28 19:32:37 +0300

    feat(detect-non-literal-regexp): support RegExp.escape
    Do not report a RegExp constructor when every dynamic part of the pattern is wrapped in RegExp.escape(), and offer a suggestion that wraps the unescaped parts.
    
    Signed-off-by: Eljees <3.14hell@gmail.com>

--- rules/detect-non-literal-regexp.js
+++ rules/detect-non-literal-regexp.js
@@ -7,6 +7,46 @@
 
 const { isStaticExpression } = require('../utils/is-static-expression');
 
+/**
+ * Checks whether the given node is a `RegExp.escape()` call.
+ *
+ * @param {import("estree").Node} node The node to check.
+ * @returns {boolean} `true` if the node is a `RegExp.escape()` call.
+ */
+function isRegExpEscapeCall(node) {
+  return (
+    node.type === 'CallExpression' &&
+    node.callee.type === 'MemberExpression' &&
+    !node.callee.computed &&
+    node.callee.object.type === 'Identifier' &&
+    node.callee.object.name === 'RegExp' &&
+    node.callee.property.type === 'Identifier' &&
+    node.callee.property.name === 'escape'
+  );
+}
+
+/**
+ * Collects the dynamic parts of an expression that are not escaped with `RegExp.escape()`.
+ * Static parts are ignored, they are handled by `isStaticExpression()`.
+ *
+ * @param {Object} params
+ * @param {import("estree").Expression} params.node The node to check.
+ * @param {import("eslint").Scope.Scope} params.scope The scope of the given node.
+ * @returns {import("estree").Expression[]} the unescaped dynamic parts.
+ */
+function getUnescapedParts({ node, scope }) {
+  if (isRegExpEscapeCall(node) || isStaticExpression({ node, scope })) {
+    return [];
+  }
+  if (node.type === 'TemplateLiteral') {
+    return node.expressions.flatMap((expression) => getUnescapedParts({ node: expression, scope }));
+  }
+  if (node.type === 'BinaryExpression' && node.operator === '+') {
+    return [...getUnescapedParts({ node: node.left, scope }), ...getUnescapedParts({ node: node.right, scope })];
+  }
+  return [node];
+}
+
 //------------------------------------------------------------------------------
 // Rule Definition
 //------------------------------------------------------------------------------
@@ -20,6 +60,7 @@ module.exports = {
       recommended: true,
       url: 'https://github.com/eslint-community/eslint-plugin-security/blob/main/docs/rules/detect-non-literal-regexp.md',
     },
+    hasSuggestions: true,
   },
   create(context) {
     const sourceCode = context.sourceCode || context.getSourceCode();
@@ -30,15 +71,23 @@ module.exports = {
           const args = node.arguments;
           const scope = sourceCode.getScope ? sourceCode.getScope(node) : context.getScope();
 
-          if (
-            args &&
-            args.length > 0 &&
-            !isStaticExpression({
-              node: args[0],
-              scope,
-            })
-          ) {
-            return context.report({ node: node, message: 'Found non-literal argument to RegExp Constructor' });
+          if (args && args.length > 0) {
+            const unescaped = getUnescapedParts({ node: args[0], scope });
+
+            if (unescaped.length > 0) {
+              return context.report({
+                node: node,
+                message: 'Found non-literal argument to RegExp Constructor',
+                suggest: [
+                  {
+                    desc: 'Escape the dynamic parts with RegExp.escape()',
+                    fix(fixer) {
+                      return unescaped.map((part) => fixer.replaceText(part, `RegExp.escape(${sourceCode.getText(part)})`));
+                    },
+                  },
+                ],
+              });
+            }
           }
         }
       },
--- test/rules/detect-non-literal-regexp.js
+++ test/rules/detect-non-literal-regexp.js
@@ -14,11 +14,52 @@ tester.run(ruleName, require(`../../rules/${ruleName}`), {
             var source = 'ab+c'
             var a = new RegExp(source, 'i')`,
     },
+    { code: "var a = new RegExp(RegExp.escape(c), 'i')" },
+    { code: "var a = new RegExp(`(${RegExp.escape(c)})$`, 'iv')" },
+    { code: "var a = new RegExp(`^prefix${RegExp.escape(c)}` + RegExp.escape(d), 'i')" },
   ],
   invalid: [
     {
       code: invalid,
-      errors: [{ message: 'Found non-literal argument to RegExp Constructor' }],
+      errors: [
+        {
+          message: 'Found non-literal argument to RegExp Constructor',
+          suggestions: [
+            {
+              desc: 'Escape the dynamic parts with RegExp.escape()',
+              output: "var a = new RegExp(RegExp.escape(c), 'i')",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      code: "var a = new RegExp(`(${c})$`, 'iv')",
+      errors: [
+        {
+          message: 'Found non-literal argument to RegExp Constructor',
+          suggestions: [
+            {
+              desc: 'Escape the dynamic parts with RegExp.escape()',
+              output: "var a = new RegExp(`(${RegExp.escape(c)})$`, 'iv')",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      code: "var a = new RegExp(`(${RegExp.escape(c)}|${d})$`, 'iv')",
+      errors: [
+        {
+          message: 'Found non-literal argument to RegExp Constructor',
+          suggestions: [
+            {
+              desc: 'Escape the dynamic parts with RegExp.escape()',
+              output: "var a = new RegExp(`(${RegExp.escape(c)}|${RegExp.escape(d)})$`, 'iv')",
+            },
+          ],
+        },
+      ],
     },
   ],
 });
